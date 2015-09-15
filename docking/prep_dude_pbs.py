992e0fc8373265d616980003114741ce020d14bc
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2015-09-14 19:06:53 -0700

    Beginning changes for running docking using pbs.

--- docking/prep_dude_pbs.py
+++ docking/prep_dude_pbs.py
@@ -33,16 +33,35 @@ def preprocess_dude(dude_dir, target):
   decoys = decoys[:1]
   hydrogenate_and_compute_partial_charges(
       receptor, "pdb", target_dir)
+  conf = """
+  receptor = receptor_hyd.pdbqt
+  ligand = %s 
+
+  center_x = 0
+  center_y = 0
+  center_z = 0
+
+  size_x = 63
+  size_y = 63
+  size_z = 63
+
+  exhaustiveness = 30
+  """
   for active in actives:
     active_file = os.path.join(actives_dir, active)
     print active_file
     hydrogenate_and_compute_partial_charges(
         active_file, "pdb", actives_dir, rigid=False)
+    active_conf = conf % active_file
+    print active_conf
   for decoy in decoys:
     decoy_file = os.path.join(decoys_dir, decoy)
     print decoy_file
     hydrogenate_and_compute_partial_charges(
         decoy_file, "pdb", decoys_dir, rigid=False)
+    decoy_conf = conf % decoy_file
+    print deocy_conf
+
 
 if __name__ == '__main__':
   args = parse_args()
