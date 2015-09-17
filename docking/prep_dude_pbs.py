753e56e519717b5750252b62e2137a8d939c862f
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2015-09-16 20:23:18 -0700

    First changes for DUD-E target prepping.

--- docking/prep_dude_pbs.py
+++ docking/prep_dude_pbs.py
@@ -6,6 +6,19 @@ import os
 import os.path
 from vs_utils.utils.nnscore_utils import hydrogenate_and_compute_partial_charges
 
+def launch_jobs(num_jobs):
+  """First attempt to factor out job launching code."""
+  # Launch jobs
+  command_list = ["python", "/home/rbharath/pbs_utils/prep_dude_pdbs.py"]
+  for job in range(num_jobs):
+    job_compounds = compounds[job*num_per_job:(job+1)*num_per_job]
+    command = " ".join(command_list + ["--compounds"] + job_compounds + ["\n"])
+    script_loc = os.path.join(script_dir, script_template % job)
+    with open(script_loc, "w") as f:
+      f.write(command)
+    qsub_command = ["qsub", "-j", "oe", "-q", "MP", "-l", "nodes=1:ppn=1", script_loc]
+    subprocess.Popen(qsub_command)
+
 def parse_args(input_args=None):
   """Parse command-line arguments."""
   parser = argparse.ArgumentParser()
@@ -13,36 +26,54 @@ def parse_args(input_args=None):
       help="Directory containing DUD-E data.")
   parser.add_argument("--target", required=1,
                       help="Name of DUD-E target.")
+  parser.add_argument("--script-dir", required=1,
+                      help="Directory to generate qsub scripts.")
+  parser.add_argument("--script-template", default="prep_dude_script%d.pbs",
+                      help="Template for script name. Must have one "
+                      "string-substitutable entry for job number.")
+  parser.add_argument("--conf-template", required=1,
+                      help="Template for configuration file. Must have two"
+                      "string-substitutable entries for receptor and ligand.")
+  parser.add_argument("--num-jobs", required=1, type=int,
+                      help="Number of PBS jobs to launch.")
+  parser.add_argument("--mode", required=1, type=string,
+                      help="Either 'launcher' or 'job'")
+  parser.add_argument('--compounds', nargs="+", required=1,
+                      help='List of dude compounds to process.')
   return parser.parse_args(input_args)
 
-def preprocess_dude(dude_dir, target):
-  """Preprocess dude compounds for docking."""
+def launch_preprocessing_jobs(dude_dir, script_dir,
+    script_template, num_jobs):
+  """Launch jobs to perform required preprocessing jobs."""
+  assert os.path.isdir(dude_dir)
   target_dir = os.path.join(dude_dir, target)
+
+  # Extract list of actives and decoys
   actives_dir = os.path.join(target_dir, "actives")
   decoys_dir = os.path.join(target_dir, "decoys")
   actives = [a for a in os.listdir(actives_dir)]
   decoys = [a for a in os.listdir(decoys_dir)]
-  receptor = os.path.join(target_dir, "receptor.pdb")
-
   # Just for debugging purposes
   actives = actives[:1]
   decoys = decoys[:1]
+
+  # Hydrogenate and process the receptor
+  receptor = os.path.join(target_dir, "receptor.pdb")
   hydrogenate_and_compute_partial_charges(
       receptor, "pdb", target_dir)
   receptor_pdbqt = os.path.join(target_dir, "receptor_hyd.pdbqt")
-  # TODO(rbharath): This is a bit of an ugly kludge.
-  conf = """receptor = %s 
-ligand = %s 
 
-center_x = 0
-center_y = 0
-center_z = 0
+  # Compute work per job
+  sources = ["active"] * len(actives) + ["decoy"] * len(decoys)
+  compounds = actives + decoys
+  num_per_job = int(math.ceil(len(compounds)/float(num_jobs)))
+  print "Number per job: %d" % num_per_job
 
-size_x = 63
-size_y = 63
-size_z = 63
 
-exhaustiveness = 30"""
+def preprocess_dude(compounds):
+  """Preprocess dude compounds for docking."""
+  with open(conf_template, "rb") as f:
+    conf = f.read()
   for active in actives:
     active_file = os.path.join(actives_dir, active)
     active_base = active.split(".")[0]
